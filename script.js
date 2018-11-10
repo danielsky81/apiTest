31fe80e011e230b2e37bf92bbd07bc59c9190fd4
danielsky <karma0rg4u@gmail.com>
2018-11-10 21:11:27 +0000

    Add marker and infoWindow to Google Map + page style to center

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 function initMap() {
+    var myPos = {
+        lat: 53.3459084,
+        lng: -6.280918
+    };
+
     var map = new google.maps.Map(document.getElementById("map"), {
-        center: {
-            lat: 53.3459084,
-            lng: -6.280918
-        },
+        center: myPos,
         zoom: 15
     });
+
+    var marker = new google.maps.Marker({
+        position: myPos,
+        map: map,
+        animation: google.maps.Animation.DROP,
+        title: "Yo!"
+    });
+
+    var contentString = ('<div id="infoWindow">' + marker.title + '</div>');
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+    });
+
 }
+
