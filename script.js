3b042fce97ee344a2c5ca3cf030ba88fb48e8acb
danielsky <karma0rg4u@gmail.com>
2018-11-10 19:52:02 +0000

    Add Google Maps API + initial styling

---
+++ script.js
@@ -0,0 +1,9 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        center: {
+            lat: 53.3459084,
+            lng: -6.280918
+        },
+        zoom: 15
+    });
+}
